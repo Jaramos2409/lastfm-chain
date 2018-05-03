c333a260f6379924bc1d7b1ee850c77d02d2db6f
Jesus Ramos <jaramos2409@gmail.com>
2018-05-03 11:01:42 -0700

    Restructured project

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark>
+          <NavbarBrand href="/" className="mr-auto">Last.fm Chain</NavbarBrand>
+        </Navbar>
+        Last.fm Chain app!
+      </div>
+    );
+  }
+}
+
+export default Header;
